eee35dd4944ea15ab324de13fb54b9bccc6eb71d
Alikhan Shorin <theololo057@gmail.com>
2024-02-20 01:28:24 +0600

    feat: :sparkles: added archiving build

--- build.py
+++ build.py
@@ -1,17 +1,25 @@
 import os
 import shutil
+import zipfile
 
 from PyInstaller.__main__ import run
 
 
 def copy_assets():
     shutil.copytree(
-        "assets/face_recognition_models", "dist/internal/face_recognition_models"
+        "assets/face_recognition_models", "dist/E-Aigaq/_internal/face_recognition_models"
     )
+    os.makedirs("dist/E-Aigaq/_internal/assets", exist_ok=True)
+    shutil.copytree("assets/fonts", "dist/E-Aigaq/_internal/assets/fonts")
 
-    os.makedirs("dist/internal/assets", exist_ok=True)
-    shutil.copytree("assets/fonts", "dist/internal/assets/fonts")
 
+def create_zip():
+    zip_file_path = os.path.join("dist", "E-Aigaq.zip")
+    with zipfile.ZipFile(zip_file_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk("dist/E-Aigaq"):
+            for file in files:
+                file_path = os.path.join(root, file)
+                zipf.write(file_path, os.path.relpath(file_path, "dist"))
 
 if __name__ == '__main__':
     options = [
@@ -24,4 +32,6 @@ if __name__ == '__main__':
 
     copy_assets()
 
-    print("Сборка завершена и файлы скопированы.")
+    create_zip()
+
+    print("Сборка завершена, файлы скопированы и создан zip-архив.")
